c6a476abb15fb8a29d23b23889a50b62b78d201a
benzammour <samir.benzammour@gmail.com>
2018-10-27 00:51:07 +0200

    adds code to insert method, fixes segfault

--- C/Datastructures/BinaryTree.c
+++ C/Datastructures/BinaryTree.c
@@ -7,37 +7,48 @@ struct Node {
 	struct Node *right;
 } Node;
 
+
+struct Node* search(struct Node* root, int value);
+void insert(struct Node* root, int value);
+void print(struct Node* root);
+struct Node* newNode(int value);
+
 struct Node* search(struct Node* root, int value) {
 	return NULL;
 }
 
-void insert(struct Node** root, int value) {
-	
+void insert(struct Node* root, int value) {
+
 	if (root == NULL) {
-		root = calloc(1, sizeof(struct Node));
-		
-		root->data = value;
-		root->left = NULL;
-		root->right = NULL;
-	} else if (value <= root->left->data) {
+		root = newNode(value);
+	} else if (value <= root->data) {
 		insert(root->left, value);
-	} else if (value > root->left->data) {
+	} else if (value > root->data) {
 		insert(root->right, value);
 	}
 }
 
 void print(struct Node* root) {
 	if (root != NULL) {
-		printf("%d ", root->data);
+		printf("%d, ", root->data);
 		print(root->left);
 		print(root->right);
 	}
 }
 
+struct Node* newNode(int value) {
+	struct Node* node = (struct Node*)calloc(1, sizeof(struct Node));
+	node->data = value;
+	node->left = NULL;
+	node->right = NULL;
+
+	return node;
+}
+
 int main() {
-	struct Node* root = calloc(1, sizeof(struct Node));
+	struct Node* root = newNode(0);
 	
-	insert(&root, 22);
+	insert(root, 99);
 
 	print(root);
 
