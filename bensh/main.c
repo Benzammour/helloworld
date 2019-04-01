1b74a0823ae0fe7afe53c7de4528b508164e3d82
benzammour <samir.benzammour@gmail.com>
2019-04-01 22:13:12 +0200

    figuring out concurrency

--- bensh/main.c
+++ bensh/main.c
@@ -9,19 +9,21 @@
 int main(int argc, char* argv[]) {
 	char* cmd = calloc(BUFFER_SIZE, sizeof(char));
 	
-	char* ls = "ls";
-	extern char* const environ[];
-	char* const command[] = {"-a", "-la"};
-	/*char* const command[] = {"-al"};*/
+	char* lsStr = "ls";
+	char* const lsCommand[] = {"ls", "-a", "-l", ".", NULL};
 	
 	while (1) {
+		printf("$ ");
 		scanf("%s", cmd);
 
-		if (strncmp(cmd, ls, 2) == 0) {
-			execve("/bin/ls", command, environ);
-			break;
+		if (strncmp(cmd, lsStr, 2) == 0) {
+			/*execve("/bin/ls", command, environ);*/
+			fork();
+			printf("test");
+			execvp("/bin/ls", lsCommand);
 		}
 
+		printf("ay");
 		/*printf("%s \n", cmd);*/
 	}
 }
